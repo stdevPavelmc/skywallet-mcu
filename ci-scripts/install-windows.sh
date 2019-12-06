80b9a7746fb61bcac8e1b48e0c09bb108f5b1b3b
Pavel Milanes (CO7WT) <dev0032@simelo.tech>
2019-12-05 22:17:57 -0500

    Refs #342, cp target referemced as folders

--- ci-scripts/install-windows.sh
+++ ci-scripts/install-windows.sh
@@ -25,21 +25,21 @@ $msys2 tree -L 3 --filelimit 100 /c/tools/
 echo "Successfully installed all tools"
 
 cd /c/tools/msys64/mingw64/bin
-cp checkmk libcheck-0.dll SDL2.dll sdl2-config libprotobuf-c-1.dll protoc-c.exe protoc-gen-c.exe /usr/bin
+cp checkmk libcheck-0.dll SDL2.dll sdl2-config libprotobuf-c-1.dll protoc-c.exe protoc-gen-c.exe /usr/bin/
 
 cd /c/tools/msys64/mingw64/include
-cp check.h check_stdint.h /usr/include
-cp -r SDL2 google protobuf-c /usr/include
+cp check.h check_stdint.h /usr/include/
+cp -r SDL2 google protobuf-c /usr/include/
 
 cd /c/tools/msys64/mingw64/lib
-cp libcheck.a libcheck.dll.a libSDL2.a libSDL2.dll.a libSDL2_test.a libSDL2main.a libprotobuf-c.a libprotobuf-c.dll.a /usr/lib
-cp pkgconfig/sdl2.pc pkgconfig/check.pc pkgconfig/libprotobuf-c.pc /usr/lib/pkgconfig
+cp libcheck.a libcheck.dll.a libSDL2.a libSDL2.dll.a libSDL2_test.a libSDL2main.a libprotobuf-c.a libprotobuf-c.dll.a /usr/lib/
+cp pkgconfig/sdl2.pc pkgconfig/check.pc pkgconfig/libprotobuf-c.pc /usr/lib/pkgconfig/
 
 cd /c/tools/msys64/mingw64/share
-cp licenses/protobuf-c/LICENSE /usr/share/licenses
-cp aclocal/check.m4 aclocal/sdl2.m4 /usr/share/aclocal
-cp -r doc/check /usr/share/doc
-cp ./info/check.info.gz /usr/share/info
+cp licenses/protobuf-c/LICENSE /usr/share/licenses/
+cp aclocal/check.m4 aclocal/sdl2.m4 /usr/share/aclocal/
+cp -r doc/check /usr/share/doc/
+cp ./info/check.info.gz /usr/share/info/
 cp ./man/man1/checkmk.1.gz /usr/share/man/man1/
 
 echo "Successfully moved all needed tools"
